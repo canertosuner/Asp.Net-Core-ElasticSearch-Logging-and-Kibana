757ffc6700ce3521ab1565eb6a4477139674a7fc
Caner Tosuner <canertosuner@outlook.com>
2018-07-29 21:01:02 +0300

    Add project files.

---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.Api.Controllers
+{
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new List<string> { "Mobile Phone", "Laptop", "Books", "Shoes" };
+    }
+}
+}
---
+++ ProductApi/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace Product.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+public void ConfigureServices(IServiceCollection services)
+{
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200/"))
+        {
+            AutoRegisterTemplate = true,
+        })
+        .CreateLogger();
+
+    services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+}
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseHsts();
+    }
+
+    loggerFactory.AddSerilog();
+
+    app.UseMvc();
+}
+    }
+}
